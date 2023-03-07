b61d0853c23c518b829e7fa3578ca5854a962cbb
Lea Ineichen <Leaineichen@bluewin.ch>
2023-03-07 16:23:06 +0100

    grassboden

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import { Background, Mushroom, Stone, Tree, Tree1 } from "./game_objects.js"
+import { Background, Mushroom, Stone, Tree, Tree1, Background1 } from "./game_objects.js"
 
 
 export default class Map {
@@ -16,7 +16,7 @@ export default class Map {
 
     // Die Hintergrundkachel wird immer hinzugefügt!!! Andere Kacheln können dann
     // darauf plaziert werden.
-    this.tiles.push( new Background(x, y) )
+    this.tiles.push( new Background1(x, y) )
     if ( tileType === "s" ) { this.tiles.push( new Stone(x, y)) }
     if ( tileType === "t" ) { this.tiles.push( new Tree(x, y)) }
     if ( tileType === "p" ) { this.tiles.push( new Mushroom(x, y)) }
