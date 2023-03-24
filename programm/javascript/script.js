fa7aa3cd955e73ff23b9d4f3f3f1cc77c115131b
e247442 <e247442@edu.sbl.ch>
2023-03-24 16:12:36 +0100

    added image loading

--- programm/javascript/script.js
+++ programm/javascript/script.js
@@ -1,8 +1,17 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("#map")
+const c = canvas.getContext("2d")
 
 canvas.width = 1024
 canvas.height = 576
 
-c.fillStyle = 'white'
+c.fillStyle = "white"
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+
+const image = new Image()
+//image.src = "current map file"
+
+image.onload = () => {
+  c.drawImage(image, -750, -550)
+}
