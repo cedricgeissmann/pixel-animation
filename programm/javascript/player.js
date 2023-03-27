bbd358c2af013887537cf1c315853e2efd54abf7
e247442 <e247442@edu.sbl.ch>
2023-03-27 09:08:41 +0200

    added player creation

---
+++ programm/javascript/player.js
@@ -0,0 +1,16 @@
+const playerImage = new Image()
+playerImage.src = "./res/player/ACharDown.png"
+
+image.onload = () => {
+    c.drawImage(
+      playerImage,
+      0,
+      0,
+      playerImage.width / 4,
+      playerImage.height,
+      canvas.width / 2 - playerImage.width / 4 / 2,
+      canvas.height / 2 - playerImage.height / 2,
+      playerImage.width / 4,
+      playerImage.height
+    )
+  }
