6c88ff40ee20a280411be857fae90235d3cc50db
e247442 <e247442@edu.sbl.ch>
2023-04-03 15:24:19 +0200

    transfered classes

--- programm/javascript/script.js
+++ programm/javascript/script.js
@@ -1,8 +1,10 @@
 import {collisions} from "../data/collisions 2.js"
+import {Boundary} from "../javascript/classes.js"
+import {Sprite} from "../javascript/classes.js"
 
 
 const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+export const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
@@ -12,20 +14,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i))
 }
 
-class Boundary {
-  static width = 48
-  static height = 48
-  constructor({ position }) {
-    this.position = position
-    this.width = 48
-    this.height = 48
-  }
-
-  draw() {
-    c.fillStyle = 'rgba(255, 0, 0, 0.0)'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
-}
 
 const boundaries = []
 const offset = {
@@ -53,32 +41,7 @@ image.src = '../res/maps/map2zoom.png'
 const playerImage = new Image()
 playerImage.src = '../res/player/ACharDown.png'
 
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position
-    this.image = image
-    this.frames = frames
 
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max
-      this.height = this.image.height
-    }
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    )
-  }
-}
 
 const player = new Sprite({
   position: {
