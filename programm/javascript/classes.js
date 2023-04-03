553cc18fd29c1c957f30a57b9541bc64ca10d4b5
e247442 <e247442@edu.sbl.ch>
2023-04-03 16:16:09 +0200

    added player-movement-animation

--- programm/javascript/classes.js
+++ programm/javascript/classes.js
@@ -17,21 +17,23 @@ export class Boundary {
   }
 
   export class Sprite {
-    constructor({ position, velocity, image, frames = { max: 1 } }) {
+    constructor({ position, velocity, image, frames = { max: 1 }, sprites }) {
       this.position = position
       this.image = image
-      this.frames = frames
+      this.frames = { ...frames, val: 0, elapsed: 0 }
   
       this.image.onload = () => {
         this.width = this.image.width / this.frames.max
         this.height = this.image.height
       }
+      this.moving = false
+      this.sprites = sprites
     }
   
     draw() {
       c.drawImage(
         this.image,
-        0,
+        this.frames.val * this.width,
         0,
         this.image.width / this.frames.max,
         this.image.height,
@@ -40,5 +42,16 @@ export class Boundary {
         this.image.width / this.frames.max,
         this.image.height
       )
+      
+    if (!this.moving) return
+
+    if (this.frames.max > 1) {
+      this.frames.elapsed++
+    }
+
+    if (this.frames.elapsed % 10 === 0) {
+      if (this.frames.val < this.frames.max - 1) this.frames.val++
+      else this.frames.val = 0
+    }
     }
   }
