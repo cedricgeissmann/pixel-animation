00111920724244cb509f0b8df824f956e2310ad8
Nicole Menzel <nicolemenzel@icloud.com>
2023-04-13 20:51:26 +0200

    Erdhaufen

--- src/map.js
+++ src/map.js
@@ -1,5 +1,5 @@
 import Game from "./game.js"
-import { Mushroom, Player, Stone, Tree, Wall, Cave, Sbackground, Tree1, Background1, Zaun, Busch2, Busch1, Wasser, Strand, Strand2, Tree2, Tree3, Blume, Blume2, kleinebüsche, Roteblume, Roterpilz, Erde, Bank1, Bank2, Brunnen1, Brunnen2, Brunnen3, Brunnen4, Essen1, Essen2, Essen3, Essen4, Hedges, Cavefloor, Mushroompurple, Caveentrance, Cavewall, StoneGrey, Lapislazuli, Ruby, Crown, Water, Background2 } from "./game_objects.js"
+import { Mushroom, Player, Stone, Tree, Wall, Cave, Sbackground, Tree1, Background1, Zaun, Busch2, Busch1, Wasser, Strand, Strand2, Tree2, Tree3, Blume, Blume2, kleinebüsche, Roteblume, Roterpilz, Erde, Bank1, Bank2, Brunnen1, Brunnen2, Brunnen3, Brunnen4, Essen1, Essen2, Essen3, Essen4, Hedges, Cavefloor, Mushroompurple, Caveentrance, Cavewall, StoneGrey, Lapislazuli, Ruby, Crown, Water, Background2, Erdhaufen, Erdhaufen2 } from "./game_objects.js"
 
 /**
  * Diese Klasse liest eine Kartendatei und erstellt die Spiel-Objekte
@@ -67,6 +67,8 @@ export default class Map {
     if ( tileType === "j" ) { new Ruby(x, y)}
     if ( tileType === "O" ) { new Crown(x, y)}
     if ( tileType === "Q" ) { new Water(x, y)}
+    if ( tileType === "g" ) { new Erdhaufen (x,y)}
+    if ( tileType === "J" ) { new Erdhaufen2 (x,y)}
 
   }
 
