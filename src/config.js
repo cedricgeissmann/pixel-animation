e31f0d23795b6be496d0dc75aca174c27e3b63c3
Servan Akçay <e227637@sbl.ch>
2023-05-06 15:03:40 +0200

    player2 movement deleted in config.js

--- src/config.js
+++ src/config.js
@@ -7,10 +7,6 @@ const config = {
     "KeyA": function() { Game.player.move("left")},
     "KeyS": function() { Game.player.move("down")},
     "KeyD": function() { Game.player.move("right")},
-    "ArrowUp": function() { Game.player2.move("up")},
-    "ArrowLeft": function() { Game.player2.move("left")},
-    "ArrowDown": function() { Game.player2.move("down")},
-    "ArrowRight": function() { Game.player2.move("right")},
     "Space": {
       callback: function() { new ShootingStone(Game.player.x / Game.tileWidth, Game.player.y / Game.tileHeight)},
       cooldown: 60 // Add cooldown in frames
